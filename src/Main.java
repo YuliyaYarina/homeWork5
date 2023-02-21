8408d96bd5191981e5d5c87b9a127401535c6ad9
Юлия <Yulechka140200@gmail.com>
2023-02-21 08:33:37 +0500

    homeWork5

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,27 @@ public class Main {
 
     public static void task1() {
         System.out.println("Задача 1");
-
+        int clientOS = 1;
+        switch (clientOS){
+            case 0:
+                System.out.println("Установите версию приложения для IOS по ссылке.");
+                break;
+            case 1:
+                System.out.println("Установите версию приложения для Андройд по ссылке.");
+                break;
+        }
     }
 
     public static void task2() {
         System.out.println("Задача 2");
+        int clientOS = 0;
+        int year = 2016;
+        boolean clientDeviceYear = year <= 2015;
+        if (clientDeviceYear) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 0) {
+            System.out.println("Установите версию приложения для IOS по ссылке.");
+        }
 
     }
 
